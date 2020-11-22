2d741d4c920d03b7f8937ee38221950fe7e6ba0f
Paul_Beckwith <303dog@gmail.com>
2020-11-22 11:53:02 -0700

    added seed data

--- portfolio-backend/db/migrate/20201121194818_create_work_exps.rb
+++ portfolio-backend/db/migrate/20201121194818_create_work_exps.rb
@@ -2,7 +2,9 @@ class CreateWorkExps < ActiveRecord::Migration[6.0]
   def change
     create_table :work_exps do |t|
       t.string :company
-      t.string :locationdates
+      t.string :location
+      t.datetime :start_date
+      t.datetime :end_date
       t.string :job_title
       t.string :job_sum
 
--- portfolio-backend/db/seeds.rb
+++ portfolio-backend/db/seeds.rb
@@ -6,3 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Program.create(title: "Project Portfolio" ,programming_lang:"Ruby/JS" , focus: "frontend" , repo: "https://github.com/303dog/js-project.git"  , creator: "Paul Beckwith" , project_date: "11/15/2020" ,)
+Program.create(title:"Family Game Night" ,programming_lang:"Ruby" , focus: "Rails" , repo: "https://github.com/303dog/rails_proj_rate-family-game_night.git"  , creator: "Paul Beckwith" , project_date: "09/15/2020")
+Program.create(title:"Home Storage" ,programming_lang:"Ruby" , focus:"Sinatra" , repo: "https://github.com/303dog/sinatra_home_storage.git"  , creator: "Paul Beckwith" , project_date: "08/15/2020")
+Program.create(title:"Top 100" ,programming_lang:"Ruby" , focus:"CLI" , repo: "https://github.com/303dog/top_100cli.git"  , creator: "Paul Beckwith" , project_date: "06/15/2020")
+
+
+Work_exp.create(company:"J.Alexanders" , location: "CO, FL" ,start_date:"08/15/2006" , end_date:"05/15/2013" ,job_title: "Manager", job_sum: "Customer relations, vendor coordinator, daily performance and execution, " )
+Work_exp.create(company: "Cheesecake Factory" , location: "AZ, CO" ,start_date:"04/15/2015" , end_date:"04/15/2017" ,job_title:"Manager" , job_sum:"Customer and staff relations, high volume performance evaluations, trouble shooting" )
+Work_exp.create(company: "Yard House", location:"CO, AZ" ,start_date:"04/15/2017" , end_date:"06/15/2020" ,job_title:"Manager" , job_sum:"Manage front and back of house staff, customer relations, security" )
